b5b4a4a4b476b94066ace0269c5dde9dcebe98a4
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2017-09-01 13:44:34 +0800

    Initial Commit

---
+++ UdpStreamer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UdpStreamer
+//
+//  Created by Osman Mesut Özcan on 1/9/2017.
+//  Copyright © 2017 Osman Mesut Özcan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
